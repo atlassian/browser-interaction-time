d44add9a2449875d79a40543b9353faadaef7ff2
Max Heinz <mheinz@atlassian.com>
2019-01-15 01:11:58 +0800

    refactor(units): rename member prop to from capital to lowercase

--- test/unit/browser-interaction-time.test.ts
+++ test/unit/browser-interaction-time.test.ts
@@ -12,24 +12,24 @@ const exec = (testTimerFn: Function) => {
  */
 describe('BrowserInteractionTime', () => {
   describe('is instantiable', () => {
-    let DefaultBrowserInteractionTime: BrowserInteractionTime
+    let defaultBrowserInteractionTime: BrowserInteractionTime
     beforeEach(() => {
-      DefaultBrowserInteractionTime = new BrowserInteractionTime({})
+      defaultBrowserInteractionTime = new BrowserInteractionTime({})
     })
 
     it('creates an instance', () => {
-      expect(DefaultBrowserInteractionTime).toBeInstanceOf(
+      expect(defaultBrowserInteractionTime).toBeInstanceOf(
         BrowserInteractionTime
       )
     })
 
     it('starts a timer', () => {
-      expect(DefaultBrowserInteractionTime.isRunning).toBeTruthy()
+      expect(defaultBrowserInteractionTime.isRunning).toBeTruthy()
     })
   })
 
   describe('API', () => {
-    let DefaultBrowserInteractionTime: BrowserInteractionTime
+    let defaultBrowserInteractionTime: BrowserInteractionTime
     let intervalCallback: TimeIntervalEllapsedCallbackData
 
     beforeEach(() => {
@@ -39,41 +39,41 @@ describe('BrowserInteractionTime', () => {
         multiplier: x => x * 2
       }
 
-      DefaultBrowserInteractionTime = new BrowserInteractionTime({
+      defaultBrowserInteractionTime = new BrowserInteractionTime({
         timeIntervalEllapsedCallbacks: [intervalCallback]
       })
     })
 
     it('.start() and .stop() returns time in milliseconds', () => {
-      DefaultBrowserInteractionTime.startTimer()
-      DefaultBrowserInteractionTime.stopTimer()
+      defaultBrowserInteractionTime.startTimer()
+      defaultBrowserInteractionTime.stopTimer()
       expect(
-        DefaultBrowserInteractionTime.getTimeInMilliseconds()
+        defaultBrowserInteractionTime.getTimeInMilliseconds()
       ).toBeDefined()
 
-      expect(DefaultBrowserInteractionTime.isRunning()).toBe(false)
+      expect(defaultBrowserInteractionTime.isRunning()).toBe(false)
     })
 
     it('.start() and .stop() multiple times returns time in milliseconds', () => {
-      DefaultBrowserInteractionTime.startTimer()
-      DefaultBrowserInteractionTime.stopTimer()
-      DefaultBrowserInteractionTime.startTimer()
-      DefaultBrowserInteractionTime.stopTimer()
+      defaultBrowserInteractionTime.startTimer()
+      defaultBrowserInteractionTime.stopTimer()
+      defaultBrowserInteractionTime.startTimer()
+      defaultBrowserInteractionTime.stopTimer()
       expect(
-        DefaultBrowserInteractionTime.getTimeInMilliseconds()
+        defaultBrowserInteractionTime.getTimeInMilliseconds()
       ).toBeDefined()
 
-      expect(DefaultBrowserInteractionTime.isRunning()).toBe(false)
+      expect(defaultBrowserInteractionTime.isRunning()).toBe(false)
     })
 
     it('.reset() returns 0 as timeInMilliseconds', () => {
-      DefaultBrowserInteractionTime.reset()
-      expect(DefaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(0)
+      defaultBrowserInteractionTime.reset()
+      expect(defaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(0)
     })
   })
 
   describe('absolute time callbacks are called when time is reached', () => {
-    let DefaultBrowserInteractionTime: BrowserInteractionTime
+    let defaultBrowserInteractionTime: BrowserInteractionTime
     let absoluteTimeEllapsedCallbacks: AbsoluteTimeEllapsedCallbackData[]
 
     beforeEach(() => {
@@ -92,7 +92,7 @@ describe('BrowserInteractionTime', () => {
         }
       ]
 
-      DefaultBrowserInteractionTime = new BrowserInteractionTime({
+      defaultBrowserInteractionTime = new BrowserInteractionTime({
         absoluteTimeEllapsedCallbacks: absoluteTimeEllapsedCallbacks
       })
     })
@@ -102,7 +102,7 @@ describe('BrowserInteractionTime', () => {
     })
 
     it('no callback is called', () => {
-      expect(DefaultBrowserInteractionTime.isRunning()).toBe(true)
+      expect(defaultBrowserInteractionTime.isRunning()).toBe(true)
       absoluteTimeEllapsedCallbacks.forEach(callbackObject => {
         expect(callbackObject.callback).not.toBeCalled()
       })
@@ -117,49 +117,49 @@ describe('BrowserInteractionTime', () => {
     })
 
     it('absoluteTimeEllapsedCallbacks are called on time', () => {
-      expect(DefaultBrowserInteractionTime.isRunning()).toBe(true)
+      expect(defaultBrowserInteractionTime.isRunning()).toBe(true)
       expect(absoluteTimeEllapsedCallbacks[0].callback).toBeFunction()
       expect(absoluteTimeEllapsedCallbacks[0].callback).not.toBeCalled()
       expect(absoluteTimeEllapsedCallbacks[1].callback).not.toBeCalled()
 
       jest.advanceTimersByTime(3000)
-      expect(DefaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(
+      expect(defaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(
         3000
       )
       expect(absoluteTimeEllapsedCallbacks[0].callback).toBeCalled()
       expect(absoluteTimeEllapsedCallbacks[1].callback).not.toBeCalled()
 
       jest.advanceTimersByTime(4000)
-      expect(DefaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(
+      expect(defaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(
         7000
       )
       expect(absoluteTimeEllapsedCallbacks[1].callback).toBeCalled()
     })
 
     it('.start() and .stop() multiple times returns time in milliseconds', () => {
-      DefaultBrowserInteractionTime.startTimer()
-      DefaultBrowserInteractionTime.stopTimer()
-      DefaultBrowserInteractionTime.startTimer()
-      DefaultBrowserInteractionTime.stopTimer()
+      defaultBrowserInteractionTime.startTimer()
+      defaultBrowserInteractionTime.stopTimer()
+      defaultBrowserInteractionTime.startTimer()
+      defaultBrowserInteractionTime.stopTimer()
       jest.advanceTimersByTime(5100)
 
       expect(
-        DefaultBrowserInteractionTime.getTimeInMilliseconds()
+        defaultBrowserInteractionTime.getTimeInMilliseconds()
       ).toBeDefined()
-      expect(DefaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(
+      expect(defaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(
         10200
       )
 
-      expect(DefaultBrowserInteractionTime.isRunning()).toBe(false)
+      expect(defaultBrowserInteractionTime.isRunning()).toBe(false)
     })
 
     it('.reset() returns 0 as timeInMilliseconds', () => {
       jest.advanceTimersByTime(4000)
-      expect(DefaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(
+      expect(defaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(
         4000
       )
-      DefaultBrowserInteractionTime.reset()
-      expect(DefaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(0)
+      defaultBrowserInteractionTime.reset()
+      expect(defaultBrowserInteractionTime.getTimeInMilliseconds()).toEqual(0)
     })
   })
 })
