178c7a9b1647b37ed0b59a317c04b904617823a9
Max Heinz <mheinz@atlassian.com>
2019-01-17 17:40:43 +1100

    test(e2e): improve integration tests

--- test/e2e/test.ts
+++ test/e2e/test.ts
@@ -2,8 +2,6 @@ import { Selector } from 'testcafe'
 
 fixture`Default`.page`./fixtures/index.html`
 
-const nameInput = Selector('#developer-name')
-
 test(`Test starts up successful`, async t => {
   const headline = Selector('.headline').innerText
   await t.expect(headline).eql('⏰browser-interaction-time')
@@ -16,14 +14,36 @@ const timerReachedAbsolute = Selector('.timer-reached-absolute').with({
   timeout: 20000
 })
 
-test(`Timer is running 1.5s`, async t => {
+const blurSelector = Selector('.tab-became-inactive').with({
+  timeout: 20000
+})
+
+const focusSelector = Selector('.tab-became-active').with({
+  timeout: 20000
+})
+
+test(`interval callbacks are called when running 1.5s`, async t => {
   await t.wait(1500)
-  await t.expect(await timerReachedInterval.count).eql(1, { timeout: 20000 })
   await t.expect(await timerReachedAbsolute.count).eql(1, { timeout: 20000 })
 })
 
-test(`Timer is running 10s`, async t => {
+test(`absolute callbacks are called when running 1.5s`, async t => {
+  await t.wait(1500)
+  await t.expect(await timerReachedAbsolute.count).eql(1, { timeout: 20000 })
+})
+
+test(`interval callbacks are called when running 10s`, async t => {
   await t.wait(10000)
   await t.expect(await timerReachedInterval.count).eql(4, { timeout: 20000 })
+})
+
+test(`absolute callbacks are called when running 10s`, async t => {
+  await t.wait(10000)
   await t.expect(await timerReachedAbsolute.count).eql(2, { timeout: 20000 })
+})
+
+test(`focus and blur callbacks are called`, async t => {
+  await t.wait(1500)
+  await t.expect(await blurSelector.count).eql(1, { timeout: 20000 })
+  await t.expect(await focusSelector.count).eql(1, { timeout: 20000 })
 })
